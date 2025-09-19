d3e5ba815268d7e13bf5528d4ea1ea363cc24010
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-09-19 20:53:03 +0300

    three tasks done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Задача 1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        // Отступ для визуального разделения задач
+        System.out.println("");
+
+        // Задача 2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        // Отступ для визуального разделения задач
+        System.out.println("");
+
+        // Задача 3
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+        // Отступ для визуального разделения задач
+        System.out.println("");
     }
 }
