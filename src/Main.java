dfca25cf02cb933ddcc580e46cfdca07c99a8d84
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-09-20 15:46:10 +0300

    corrected tasks 5-10

--- src/Main.java
+++ src/Main.java
@@ -32,28 +32,30 @@ public class Main {
 
         // Задача 5
         for (int i = 1904; i <= 2096; i = i + 4) {
-            System.out.println(i);
+            System.out.println(i + " год является високосным");
         }
         // Отступ для визуального разделения задач
         System.out.println("");
 
         // Задача 6
         for (int i = 7; i <= 98; i = i + 7) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
         // Отступ для визуального разделения задач
+        System.out.println("");
         System.out.println("");
 
         // Задача 7
         for (int i = 1; i <= 512; i = i * 2) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
         // Отступ для визуального разделения задач
+        System.out.println("");
         System.out.println("");
 
         // Задача 8
         for (int i = 1; i <= 12; i++) {
-            System.out.println(29_000*i);
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + 29_000*i + " рублей");
         }
         // Отступ для визуального разделения задач
         System.out.println("");
@@ -61,15 +63,15 @@ public class Main {
         // Задача 9
         double money = 29_000;
         for (int i = 1; i <= 12; i++) {
-            money = money * 1.12;
-            System.out.println(money);
+            money = Math.round(money * 1.01 * 100D) / 100D;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + money + " рублей");
         }
         // Отступ для визуального разделения задач
         System.out.println("");
 
         // Задача 10
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i*2);
+            System.out.println("2 * " + i + " = " + 2*i);
         }
         // Отступ для визуального разделения задач
         System.out.println("");
